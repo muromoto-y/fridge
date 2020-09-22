79ed11c844cab2809b578fd45fec0cff00253fa7
muromoto-y <6610010@gmail.com>
2020-09-22 15:31:53 +0900

    ストロングパラメーター

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -4,4 +4,10 @@ class CookingsController < ApplicationController
     @cookings = Cooking.all
   end
 
+  private
+  
+  def cooking_params
+    params.require(:cooking).permit(:image, :cooking_name, :text, :material1, :quantity1, :material2, :quantity2, :material3, :quantity3, :material4, :quantity4, :material5, :quantity5, :material6, :quantity6, :material7, :quantity17, :material8, :quantity8, :material9, :quantity9, :material0, :quantity0, :name).merge(user_id: current_user.id)
+  end
+
 end
