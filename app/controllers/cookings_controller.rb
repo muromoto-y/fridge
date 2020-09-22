45477197aab55f1326a5c0d45a5b37c84a08ecb0
muromoto-y <6610010@gmail.com>
2020-09-22 17:26:00 +0900

    項目完成

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -4,8 +4,21 @@ class CookingsController < ApplicationController
     @cookings = Cooking.all
   end
 
+  def new
+    @cooking = Cooking.new
+  end
+
+  def create
+    @cooking = Cooking.new(cooking_params)
+    if @cooking.save
+       redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
-  
+
   def cooking_params
     params.require(:cooking).permit(:image, :cooking_name, :text, :material1, :quantity1, :material2, :quantity2, :material3, :quantity3, :material4, :quantity4, :material5, :quantity5, :material6, :quantity6, :material7, :quantity17, :material8, :quantity8, :material9, :quantity9, :material0, :quantity0, :name).merge(user_id: current_user.id)
   end
