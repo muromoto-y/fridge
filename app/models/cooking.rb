88a1fa2f659d057d33a7f52617156a46deacbf6c
muromoto-y <6610010@gmail.com>
2020-09-22 21:26:39 +0900

    完成ビューは後で

--- app/models/cooking.rb
+++ app/models/cooking.rb
@@ -4,4 +4,12 @@ class Cooking < ApplicationRecord
   has_many :users, through: :bookmarks
   has_one_attached :image
 
+  with_options presence: true do
+    validates :cooking_name
+    validates :explanation
+    validates :ingredients1 
+    validates :quantity1 
+  end
+
+
 end
