6e09585c08f8e57eb9671585dd1dc48b703f083f
muromoto-y <6610010@gmail.com>
2020-09-22 15:03:26 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cookings, through: :bookmarks
+  has_many :bookmarks
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+  with_options presence: true do
+    validates :nick_name
+    validates :password,            length: {minimum: 6 },
+                                    format: { with: VALID_PASSWORD_REGEX},
+                                    confirmation: true
+    validates :email,               uniqueness: true
+
+  end
 end
